15190498e8737996afb865fc813e3999a74349fa
Lambda_School_Loaner_191 <lambda_school_loaner_191@lambdasr191smbp.home>
2019-10-03 23:22:43 -0700

    added images for wall

--- landing-pages/src/components/landing/LandingPage.js
+++ landing-pages/src/components/landing/LandingPage.js
@@ -1,7 +1,13 @@
 import React from "react";
-import FilmsImg from "../images/films.png";
-import MusicImg from "../images/music.png";
-import SportsImg from "../images/sports.png";
+import MusicImg from "../images/musicwall.jpg";
+import FilmsImg from "../images/filmswall2.jpg";
+import VilliansImg from "../images/villainswall.jpg";
+import DestinationImg from "../images/destinationwall.jpg";
+import VideoGameImg from "../images/videogamewall.jpg";
+import HerosImg from "../images/heroswall.jpg";
+import MoviesImg from "../images/movieswall.jpg";
+import TvImg from "../images/tvwall.jpg";
+import SportsImg from "../images/sportswall.jpg";
 import SportsIcon from "../images/sportsicon.png";
 import MusicIcon from "../images/musicicon.png";
 import CreateImg from "../images/user12.png";
@@ -54,11 +60,11 @@ class LandingPage extends React.Component {
             </div>
 
             <Carousel
-              autoPlay
+              // autoPlay
               // dynamicHeight={true}
               infiniteLoop
               showArrows={false}
-              interval={4000}
+              interval={3000}
               transitionTime={900}
               showThumbs={false}
               showStatus={false}
@@ -134,29 +140,28 @@ class LandingPage extends React.Component {
 
           {/* Middle Content */}
           <section className="middle-content">
-            <div className="banner-categories">
-              <img className="banner-img" src={MusicIcon} alt=""></img>
-              <img className="banner-img" src={MusicIcon} alt=""></img>
-              <img className="banner-img" src={MusicIcon} alt=""></img>
-              <img className="banner-img" src={MusicIcon} alt=""></img>
-              <img className="banner-img" src={MusicIcon} alt=""></img>
-              <img className="banner-img" src={MusicIcon} alt=""></img>
-              <img className="banner-img" src={MusicIcon} alt=""></img>
-              <img className="banner-img" src={MusicIcon} alt=""></img>
-              <img className="banner-img" src={MusicIcon} alt=""></img>
-            </div>
-            <div className="card-columns">
+            <div className="card-container">
               <div className="card">
                 <img
                   className="card-img-top"
-                  src={MusicIcon}
+                  src={MusicImg}
                   alt="Card image cap"
-                  style={{ width: "150px", height: "150px" }}
                 />
                 <div className="card-body">
-                  <h5 className="card-title">
-                    Card title that wraps to a new line
-                  </h5>
+                  <h3 className="card-title">Music Artists</h3>
+                  <p className="card-text">
+                    This is a longer card with supporting text below as a
+                  </p>
+                </div>
+              </div>
+              <div className="card">
+                <img
+                  className="card-img-top"
+                  src={SportsImg}
+                  alt="Card image cap"
+                />
+                <div className="card-body">
+                  <h3 className="card-title">Sports Teams</h3>
                   <p className="card-text">
                     This is a longer card with supporting text below as a
                     natural lead-in to additional content. This content is a
@@ -164,100 +169,93 @@ class LandingPage extends React.Component {
                   </p>
                 </div>
               </div>
-              <div className="card p-3">
-                <blockquote className="blockquote mb-0 card-body">
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Integer posuere erat a ante.
+              <div className="card">
+                <img
+                  className="card-img-top"
+                  src={MoviesImg}
+                  alt="Card image cap"
+                />
+                <div className="card-body">
+                  <h3 className="card-title">Movies</h3>
+                  <p className="card-text">
+                    This is a longer card with supporting text below as a
+                    natural lead-in to additional content. This content is a
+                    little bit longer.
                   </p>
-                  <footer className="blockquote-footer">
-                    <small className="text-muted">
-                      Someone famous in{" "}
-                      <cite title="Source Title">Source Title</cite>
-                    </small>
-                  </footer>
-                </blockquote>
+                </div>
               </div>
               <div className="card">
                 <img
                   className="card-img-top"
-                  src=".../100px160/"
+                  src={TvImg}
                   alt="Card image cap"
                 />
                 <div className="card-body">
-                  <h5 className="card-title">Card title</h5>
+                  <h3 className="card-title">TV Shows</h3>
                   <p className="card-text">
-                    This card has supporting text below as a natural lead-in to
-                    additional content.
-                  </p>
-                  <p className="card-text">
-                    <small className="text-muted">
-                      Last updated 3 mins ago
-                    </small>
-                  </p>
-                </div>
-              </div>
-              <div className="card bg-primary text-white text-center p-3">
-                <blockquote className="blockquote mb-0">
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Integer posuere erat.
-                  </p>
-                  <footer className="blockquote-footer">
-                    <small>
-                      Someone famous in{" "}
-                      <cite title="Source Title">Source Title</cite>
-                    </small>
-                  </footer>
-                </blockquote>
-              </div>
-              <div className="card text-center">
-                <div className="card-body">
-                  <h5 className="card-title">Card title</h5>
-                  <p className="card-text">
-                    This card has a regular title and short paragraphy of text
-                    below it.
-                  </p>
-                  <p className="card-text">
-                    <small className="text-muted">
-                      Last updated 3 mins ago
-                    </small>
+                    This is a longer card with supporting text below as a
+                    natural lead-in to additional content. This content is a
+                    little bit longer.
                   </p>
                 </div>
               </div>
               <div className="card">
                 <img
-                  className="card-img"
-                  src=".../100px260/"
-                  alt="Card image"
+                  className="card-img-top"
+                  src={DestinationImg}
+                  alt="Card image cap"
                 />
-              </div>
-              <div className="card p-3 text-right">
-                <blockquote className="blockquote mb-0">
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Integer posuere erat a ante.
+                <div className="card-body">
+                  <h3 className="card-title">Destinations</h3>
+                  <p className="card-text">
+                    This is a longer card with supporting text below as a
+                    natural lead-in to additional content. This content is a
+                    little bit longer.
                   </p>
-                  <footer className="blockquote-footer">
-                    <small className="text-muted">
-                      Someone famous in{" "}
-                      <cite title="Source Title">Source Title</cite>
-                    </small>
-                  </footer>
-                </blockquote>
+                </div>
               </div>
               <div className="card">
+                <img
+                  className="card-img-top"
+                  src={VilliansImg}
+                  alt="Card image cap"
+                />
                 <div className="card-body">
-                  <h5 className="card-title">Card title</h5>
+                  <h3 className="card-title">Villains</h3>
                   <p className="card-text">
-                    This is another card with title and supporting text below.
-                    This card has some additional content to make it slightly
-                    taller overall.
+                    This is a longer card with supporting text below as a
+                    natural lead-in to additional content. This content is a
+                    little bit longer.
                   </p>
+                </div>
+              </div>
+              <div className="card">
+                <img
+                  className="card-img-top"
+                  src={VideoGameImg}
+                  alt="Card image cap"
+                />
+                <div className="card-body">
+                  <h3 className="card-title">Video Games</h3>
                   <p className="card-text">
-                    <small className="text-muted">
-                      Last updated 3 mins ago
-                    </small>
+                    This is a longer card with supporting text below as a
+                    natural lead-in to additional content. This content is a
+                    little bit longer.
+                  </p>
+                </div>
+              </div>
+              <div className="card">
+                <img
+                  className="card-img-top"
+                  src={HerosImg}
+                  alt="Card image cap"
+                />
+                <div className="card-body">
+                  <h3 className="card-title">Heros</h3>
+                  <p className="card-text">
+                    This is a longer card with supporting text below as a
+                    natural lead-in to additional content. This content is a
+                    little bit longer.
                   </p>
                 </div>
               </div>
