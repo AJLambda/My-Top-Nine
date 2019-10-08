2e8eb32bcfd76acd6f6b75f02d5b4001f2e68769
Lambda_School_Loaner_191 <lambda_school_loaner_191@lambdasr191smbp.home>
2019-10-08 00:41:05 -0700

    working on footer:

--- landing-pages/src/components/footer/Footer.js
+++ landing-pages/src/components/footer/Footer.js
@@ -1,11 +1,76 @@
 import React from "react";
+import {
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  FormText,
+  InputGroup,
+  FormControl
+} from "react-bootstrap";
 import "../landing/landing.scss";
 
 class Footer extends React.Component {
   render() {
     return (
       <footer>
-        <p>&copy 2019 My Top 9</p>
+        <div className="footer-top">
+          <div className="footer-top-left">
+            <div className="started">
+              <h4>Getting Started</h4>
+              <ul>
+                <li>Starting your wall</li>
+                <li>Selecting categories</li>
+              </ul>
+            </div>
+            <div className="about">
+              <h4>About Us</h4>
+              <ul>
+                <li>Our Ethos</li>
+                <li>How It Works</li>
+                <li>FAQ</li>
+              </ul>
+            </div>
+            <div className="contact">
+              <h4>Connect</h4>
+              <ul>
+                <li>Contact Us</li>
+                <li>Facebook</li>
+                <li>Twitter</li>
+                <li>Instagram</li>
+              </ul>
+            </div>
+          </div>
+          <div className="footer-top-right">
+            <h4>Join our newsletter</h4>
+            {/* <h4>Email</h4> */}
+            <label htmlFor="email">Email</label>
+            <InputGroup size="lg" className="mb-3">
+              <FormControl
+                placeholder="Recipient's email"
+                aria-label="Recipient's email"
+                aria-describedby="basic-addon2"
+                id="email"
+              />
+              <InputGroup.Append>
+                <Button variant="outline-secondary">Register</Button>
+              </InputGroup.Append>
+            </InputGroup>
+          </div>
+        </div>
+        <div className="footer-bottom">
+          <div className="footer-bottom-left">
+            <p>
+              <span>©</span>2019 My Top Nine
+            </p>
+          </div>
+          <div className="footer-bottom-right">
+            <a>Terms</a>
+            <a>Privacy</a>
+            <a></a>
+          </div>
+        </div>
       </footer>
     );
   }
