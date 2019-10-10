df6a260b3c55a4c3a539baaca96511e606918f37
Lambda_School_Loaner_191 <lambda_school_loaner_191@lambdasr191smbp.home>
2019-10-09 21:50:53 -0700

    working on buttons

--- landing-pages/src/components/footer/Footer.js
+++ landing-pages/src/components/footer/Footer.js
@@ -1,6 +1,5 @@
 import React from "react";
 import {
-  Button,
   Form,
   FormGroup,
   Label,
@@ -13,98 +12,117 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFacebookF } from "@fortawesome/free-brands-svg-icons";
 import { faTwitter } from "@fortawesome/free-brands-svg-icons";
 import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
+import Icon from "@material-ui/core/Icon";
+import SaveIcon from "@material-ui/icons/Save";
 import "../landing/landing.scss";
 
-class Footer extends React.Component {
-  render() {
-    return (
-      <footer>
-        <div className="footer-top">
-          <div className="footer-top-left">
-            <div className="started">
-              <h4>Getting Started</h4>
-              <ul>
-                <li>Starting your wall</li>
-                <li>Selecting categories</li>
-              </ul>
-            </div>
-            <div className="about">
-              <h4>About Us</h4>
-              <ul>
-                <li>Our Ethos</li>
-                <li>How It Works</li>
-                <li>FAQ</li>
-              </ul>
-            </div>
-            <div className="contact">
-              <h4>Connect</h4>
-              <ul>
-                <li>Contact Us</li>
-                <li>
-                  <FontAwesomeIcon
-                    icon={faTwitter}
-                    size={"xs"}
-                    style={{ marginRight: "3px" }}
-                  />
-                  Twitter
-                </li>
-                <li style={{ fontSize: "13px" }}>
-                  <FontAwesomeIcon
-                    icon={faFacebookF}
-                    size={"xs"}
-                    style={{ marginRight: "6px", marginLeft: "1px" }}
-                  />
-                  Facebook
-                </li>
-                <li>
-                  <FontAwesomeIcon
-                    icon={faInstagram}
-                    size={"xs"}
-                    style={{ marginRight: "5px" }}
-                  />
-                  Instagram
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div className="footer-top-right">
-            <h4>Join our newsletter</h4>
-            {/* <h4>Email</h4> */}
-            <label htmlFor="email">Email</label>
-            <InputGroup size="lg" className="mb-3">
-              <FormControl
-                placeholder="email@address.com"
-                aria-label="email@address.com"
-                aria-describedby="basic-addon2"
-                id="email"
-              />
-              <InputGroup.Append>
-                <Button
-                  className="register-btn"
-                  variant="outline-secondary"
-                  style={{ backgroundColor: "firebrick", color: "white" }}
-                >
-                  REGISTER
-                </Button>
-              </InputGroup.Append>
-            </InputGroup>
-          </div>
-        </div>
-        <div className="footer-bottom">
-          <div className="footer-bottom-left">
-            <p>
-              <span>©</span>2019 My Top Nine
-            </p>
-          </div>
-          <div className="footer-bottom-right">
-            <a>Terms</a>
-            <a>Privacy</a>
-            <a>Security</a>
-          </div>
-        </div>
-      </footer>
-    );
+const useStyles = makeStyles(theme => ({
+  button: {
+    margin: "0px",
+    borderTopLeftRadius: "0px",
+    borderBottomLeftRadius: "0px",
+    fontFamily: "Nunito"
+    // boxShadow: "none"
   }
-}
+}));
 
-export default Footer;
+export default function Footer() {
+  const classes = useStyles();
+  return (
+    <footer>
+      <div className="footer-top">
+        <div className="footer-top-left">
+          <div className="started">
+            <h4>Getting Started</h4>
+            <ul>
+              <li>Starting your wall</li>
+              <li>Selecting categories</li>
+            </ul>
+          </div>
+          <div className="about">
+            <h4>About Us</h4>
+            <ul>
+              <li>Our Ethos</li>
+              <li>How It Works</li>
+              <li>FAQ</li>
+            </ul>
+          </div>
+          <div className="contact">
+            <h4>Connect</h4>
+            <ul>
+              <li>Contact Us</li>
+              <li>
+                <FontAwesomeIcon
+                  icon={faTwitter}
+                  size={"xs"}
+                  style={{ marginRight: "3px" }}
+                />
+                Twitter
+              </li>
+              <li style={{ fontSize: "13px" }}>
+                <FontAwesomeIcon
+                  icon={faFacebookF}
+                  size={"xs"}
+                  style={{ marginRight: "6px", marginLeft: "1px" }}
+                />
+                Facebook
+              </li>
+              <li>
+                <FontAwesomeIcon
+                  icon={faInstagram}
+                  size={"xs"}
+                  style={{ marginRight: "5px" }}
+                />
+                Instagram
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="footer-top-right">
+          <h4>Join our newsletter</h4>
+          {/* <h4>Email</h4> */}
+          <label htmlFor="email">Email</label>
+          <InputGroup size="lg" className="mb-3">
+            <FormControl
+              placeholder="email@address.com"
+              aria-label="email@address.com"
+              aria-describedby="basic-addon2"
+              id="email"
+            />
+            <InputGroup.Append>
+              <Button
+                variant="contained"
+                color="primary"
+                className={classes.button}
+              >
+                REGISTER
+                <span>
+                  <FontAwesomeIcon
+                    icon={faPaperPlane}
+                    size={"lg"}
+                    style={{ marginLeft: "5px" }}
+                  />
+                </span>
+              </Button>
+            </InputGroup.Append>
+          </InputGroup>
+        </div>
+      </div>
+      <div className="footer-bottom">
+        <div className="footer-bottom-left">
+          <p>
+            <span>©</span>2019 My Top Nine
+          </p>
+        </div>
+        <div className="footer-bottom-right">
+          <a>Terms</a>
+          <a>Privacy</a>
+          <a>Security</a>
+        </div>
+      </div>
+    </footer>
+  );
+}
