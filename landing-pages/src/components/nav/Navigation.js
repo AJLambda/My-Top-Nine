5fade15f740048fd4497bc76291eb254ba0c21a6
Lambda_School_Loaner_191 <lambda_school_loaner_191@lambdasr191smbp.home>
2019-10-17 20:41:17 -0700

    added hamburger menu mobile

--- landing-pages/src/components/nav/Navigation.js
+++ landing-pages/src/components/nav/Navigation.js
@@ -3,11 +3,12 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import NavLogo from "../images/logo6.png";
 import Underline6 from "../images/underline6.png";
-import { makeStyles } from "@material-ui/core/styles";
+import { withStyles } from "@material-ui/core/styles";
 import { Link } from "react-scroll";
 import Button from "@material-ui/core/Button";
+import { slide as Menu } from "react-burger-menu";
 
-const useStyles = makeStyles(theme => ({
+const styles = theme => ({
   navBtn: {
     width: "200px",
     height: "50px",
@@ -39,110 +40,164 @@ const useStyles = makeStyles(theme => ({
       color: "white"
     }
   }
-}));
+});
 
-export default function Navigation() {
-  const classes = useStyles();
-  return (
-    <>
-      <Navbar className="nav">
-        <Navbar.Brand href="#">
-          <img
-            src={NavLogo}
-            width="70"
-            height="70"
-            className="d-inline-block align-bottom"
-            alt="React Bootstrap logo"
-          />
-        </Navbar.Brand>
-        <a className="logo" href="/">
-          My Top Nine
-          <img className="underline-logo" src={Underline6} alt="" />
-        </a>
+const showSettings = event => {
+  event.preventDefault();
+};
 
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav style={{ margin: "0 auto" }}>
-            <Link
-              activeClass="active"
-              className="team"
-              to="team"
-              spy={true}
-              smooth={true}
-              offset={-40}
-            >
-              <Button
-                size="large"
-                variant="outlined"
-                className={classes.button}
+class Navigation extends React.Component {
+  state = {
+    searchNodes: ""
+  };
+  // const classes = useStyles();
+  render() {
+    const { classes } = this.props;
+    return (
+      <>
+        <Menu>
+          <Link
+            activeClass="active"
+            className="team"
+            to="team"
+            spy={true}
+            smooth={true}
+            offset={-40}
+          >
+            About Us
+          </Link>
+          <Link
+            activeClass="active"
+            className="what"
+            to="what"
+            spy={true}
+            smooth={true}
+            offset={-40}
+          >
+            Introduction
+          </Link>
+          <Link
+            activeClass="active"
+            className="works"
+            to="works"
+            spy={true}
+            smooth={true}
+            offset={-40}
+          >
+            How It Works
+          </Link>
+          <Link
+            activeClass="active"
+            className="wall"
+            to="wall"
+            spy={true}
+            smooth={true}
+            offset={-80}
+          >
+            Top Nine Wall
+          </Link>
+        </Menu>
+        <Navbar className="nav">
+          <Navbar.Brand href="#">
+            <img
+              src={NavLogo}
+              width="70"
+              height="70"
+              className="d-inline-block align-bottom"
+              alt="React Bootstrap logo"
+            />
+          </Navbar.Brand>
+          <a className="logo" href="/">
+            My Top Nine
+            <img className="underline-logo" src={Underline6} alt="" />
+          </a>
+
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav style={{ margin: "0 auto" }}>
+              <Link
+                activeClass="active"
+                className="team"
+                to="team"
+                spy={true}
+                smooth={true}
+                offset={-40}
               >
-                About Us
-              </Button>
-            </Link>
-            <Link
-              activeClass="active"
-              className="what"
-              to="what"
-              spy={true}
-              smooth={true}
-              offset={-40}
-            >
-              <Button
-                size="large"
-                variant="outlined"
-                className={classes.button}
-              >
-                intro
-              </Button>
-            </Link>
-            <Link
-              activeClass="active"
-              className="works"
-              to="works"
-              spy={true}
-              smooth={true}
-              offset={-40}
-            >
-              <Button
-                size="large"
-                variant="outlined"
-                className={classes.button}
-              >
-                How it Works
-              </Button>
-            </Link>
-            <Link
-              activeClass="active"
-              className="wall"
-              to="wall"
-              spy={true}
-              smooth={true}
-              offset={-80}
-            >
-              <Button
-                size="large"
-                variant="outlined"
-                className={classes.button}
-              >
-                Top nine wall
-              </Button>
-            </Link>
-          </Nav>
-          <Nav>
-            <Nav.Link href="/">
-              <div className="nav-btn">
                 <Button
-                  variant="contained"
-                  aria-label="add"
-                  className={classes.navBtn}
+                  size="large"
+                  variant="outlined"
+                  className={classes.button}
                 >
-                  GET STARTED
+                  About Us
                 </Button>
-              </div>
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    </>
-  );
+              </Link>
+              <Link
+                activeClass="active"
+                className="what"
+                to="what"
+                spy={true}
+                smooth={true}
+                offset={-40}
+              >
+                <Button
+                  size="large"
+                  variant="outlined"
+                  className={classes.button}
+                >
+                  intro
+                </Button>
+              </Link>
+              <Link
+                activeClass="active"
+                className="works"
+                to="works"
+                spy={true}
+                smooth={true}
+                offset={-40}
+              >
+                <Button
+                  size="large"
+                  variant="outlined"
+                  className={classes.button}
+                >
+                  How it Works
+                </Button>
+              </Link>
+              <Link
+                activeClass="active"
+                className="wall"
+                to="wall"
+                spy={true}
+                smooth={true}
+                offset={-80}
+              >
+                <Button
+                  size="large"
+                  variant="outlined"
+                  className={classes.button}
+                >
+                  Top nine wall
+                </Button>
+              </Link>
+            </Nav>
+            <Nav>
+              <Nav.Link href="/">
+                <div className="nav-btn">
+                  <Button
+                    variant="contained"
+                    aria-label="add"
+                    className={classes.navBtn}
+                  >
+                    GET STARTED
+                  </Button>
+                </div>
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </>
+    );
+  }
 }
+
+export default withStyles(styles, { withTheme: true })(Navigation);
